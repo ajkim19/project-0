8743678a3b5a5c65cf982c60c34309e2d74bfca7
ajkim19 <ajkim19@gmail.com>
2020-01-10 09:21:57 -0600

    Connecting input to sqlite

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"strconv"
 	"bufio"
 	"database/sql"
 	"fmt"
-	"github.com/mattn/go-sqlite3"
 	"os"
-	"database/sql"
+	"strconv"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	database, _ := sql.Open(driverName: "sqlite3", dataSourceName: "./journal.db")
-	statement, _ := database.Prepare(query: "CREATE TABLE IF NOT EXISTS journal_entries (id INTEGER PRIMARY KEY, date TEXT, entry TEXT)")
+	//Creates map for journal entries
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Input Journal Entry:")
+	journalEntry, _ := reader.ReadString('\n')
+	journalDate := string(time.Now().Format("01-02-2006"))
+
+	database, _ := sql.Open("sqlite3", "./journal.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS journal_entries (id INTEGER PRIMARY KEY, date TEXT, entry TEXT)")
 	statement.Exec()
-	statement, _ = database.Prepare(query: "INSERT INTO journal_entries (date, entry) VALUES (?, ?)")
-	statement.Exec(args: "01-01-2020", "It is New Year's Day today.")
-	rows, _ := database.Query(query: "SELECT * FROM journal_entries")
+	statement, _ = database.Prepare("INSERT INTO journal_entries (date, entry) VALUES (?, ?)")
+	statement.Exec(journalDate, journalEntry)
+	rows, _ := database.Query("SELECT * FROM journal_entries")
 	var id int
 	var date string
 	var entry string
@@ -25,17 +32,5 @@ func main() {
 		rows.Scan(&id, &date, &entry)
 		fmt.Println(strconv.Itoa(id) + ": " + date + " " + entry)
 	}
-	
 
-	//Creates map for journal entries
-	journalEntries := map[string]string{}
-
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("Input Journal Entry:")
-	text, _ := reader.ReadString('\n')
-	journalDate := string(time.Now().Format("01-02-2006"))
-	journalEntries[journalDate] = text
-
-	fmt.Println(journalEntries)
 }
