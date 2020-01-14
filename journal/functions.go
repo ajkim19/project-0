cc24b1d3b6d16eec890c726b6c204fea3c774914
ajkim19 <ajkim19@gmail.com>
2020-01-14 14:57:32 -0600

    Adding journal package
    The journal folder was created for the package journal. Functions from main.go were moved to functions.go. main.go now imports the functions from the journal package. There is now a switch case conditional for the use of the functions.

---
+++ journal/functions.go
@@ -0,0 +1,121 @@
+package journal
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+)
+
+var id int
+var date string
+var entry string
+
+// InputEntry adds journal entry into database
+func InputEntry(d *sql.DB) {
+	// Prompts user for journal input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input journal entry:")
+	journalEntry, _ := reader.ReadString('\n')
+	journalEntry = journalEntry[:len(journalEntry)-1]
+
+	// Grabs the date of the entry made
+	journalDate := string(time.Now().Format("01-02-2006"))
+
+	rows, _ := d.Query("SELECT * FROM journal_entries")
+	dateExists := false
+
+	for rows.Next() {
+		rows.Scan(&id, &date, &entry)
+		if journalDate == date {
+			dateExists = true
+		}
+	}
+
+	if dateExists {
+		rows, _ = d.Query("SELECT * FROM journal_entries")
+		rows.Scan(&id, &date, &entry)
+		journalEntry = fmt.Sprint(entry + "\n\n" + journalEntry)
+
+		// Adds entry onto the entry with the same date into the database
+		statement, _ := d.Prepare("UPDATE journal_entries SET entry = ? WHERE date = ?")
+		statement.Exec(journalEntry, journalDate)
+
+	} else {
+		// Inserts date and entry into the database
+		statement, _ := d.Prepare("INSERT INTO journal_entries (date, entry) VALUES (?, ?)")
+		statement.Exec(journalDate, journalEntry)
+	}
+
+	rows, _ = d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
+	rows.Scan(&id, &date, &entry)
+	fmt.Println(date + ": " + entry)
+}
+
+// ViewEntry prints the entry of a particular date
+func ViewEntry(d *sql.DB) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input date of journal entry to view:")
+	journalDate, _ := reader.ReadString('\n')
+	journalDate = journalDate[:len(journalDate)-1]
+
+	rows, _ := d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
+
+	for rows.Next() {
+		rows.Scan(&id, &date, &entry)
+		fmt.Println(date + ": " + entry)
+	}
+}
+
+// ViewEntireJournal prints the entire table of journal_entries
+func ViewEntireJournal(d *sql.DB) {
+	rows, _ := d.Query("SELECT * FROM journal_entries")
+	for rows.Next() {
+		rows.Scan(&id, &date, &entry)
+		fmt.Println(date + ": " + entry)
+	}
+}
+
+// DeleteEntry deletes the entry of a particular date
+func DeleteEntry(d *sql.DB) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input date of journal entry to delete:")
+	journalDate, _ := reader.ReadString('\n')
+
+	statement, _ := d.Prepare("DELETE FROM journal_entries WHERE date = ?")
+	statement.Exec(journalDate)
+}
+
+// DeleteTable deletes the entire table of journal_entries
+func DeleteTable(d *sql.DB) {
+	statement, _ := d.Prepare("DROP TABLE journal_entries")
+	statement.Exec()
+}
+
+// EditEntry replaces the entry of a particular date
+func EditEntry(d *sql.DB) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input date of journal entry to edit:")
+	journalDate, _ := reader.ReadString('\n')
+	journalDate = journalDate[:len(journalDate)-1]
+
+	rows, _ := d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
+	for rows.Next() {
+		rows.Scan(&id, &date, &entry)
+		fmt.Println(date + ": " + entry)
+	}
+
+	fmt.Println("Input replacement entry:")
+	journalEntry, _ := reader.ReadString('\n')
+	journalEntry = journalEntry[:len(journalEntry)-1]
+
+	statement, _ := d.Prepare("UPDATE journal_entries SET entry = ? WHERE date = ?")
+	statement.Exec(journalEntry, journalDate)
+
+	rows, _ = d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
+	for rows.Next() {
+		rows.Scan(&id, &date, &entry)
+		fmt.Println(date + ": " + entry)
+	}
+}
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
-	"fmt"
-	"os"
-	"strconv"
-	"time"
 
+	"github.com/ajkim19/project-0/journal"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -19,117 +15,24 @@ func main() {
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS journal_entries (id INTEGER PRIMARY KEY, date TEXT, entry TEXT)")
 	statement.Exec()
 
-	//inputEntry(database)
-	//printEntireJournal(database)
-	//searchEntry(database)
-	//editEntry(database)
-}
+	var view bool
+	var delete bool
+	var edit bool
+	var all bool
 
-var id int
-var date string
-var entry string
-
-// Adds journal entry into database
-func inputEntry(d *sql.DB) {
-	// Prompts user for journal input
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input journal entry:")
-	journalEntry, _ := reader.ReadString('\n')
-	journalEntry = journalEntry[:len(journalEntry)-1]
-
-	// Grabs the date of the entry made
-	journalDate := string(time.Now().Format("01-02-2006"))
-
-	rows, _ := d.Query("SELECT * FROM journal_entries")
-	dateExists := false
-
-	for rows.Next() {
-		rows.Scan(&id, &date, &entry)
-		if journalDate == date {
-			dateExists = true
-		}
+	switch {
+	case view == true:
+		journal.ViewEntry(database)
+	case view == true && all == true:
+		journal.ViewEntireJournal(database)
+	case delete == true:
+		journal.DeleteEntry(database)
+	case delete == true && all == true:
+		journal.DeleteTable(database)
+	case edit == true:
+		journal.EditEntry(database)
+	default:
+		journal.InputEntry(database)
 	}
 
-	if dateExists {
-		rows, _ = d.Query("SELECT * FROM journal_entries")
-		rows.Scan(&id, &date, &entry)
-		journalEntry = fmt.Sprint(entry + "\n\n" + journalEntry)
-
-		// Inserts date and entry into the database
-		statement, _ := d.Prepare("UPDATE journal_entries SET entry = ? WHERE date = ?")
-		statement.Exec(journalEntry, journalDate)
-
-	} else {
-		// Inserts date and entry into the database
-		statement, _ := d.Prepare("INSERT INTO journal_entries (date, entry) VALUES (?, ?)")
-		statement.Exec(journalDate, journalEntry)
-	}
-}
-
-// Prints entire table of journal_entries
-func printEntireJournal(d *sql.DB) {
-	rows, _ := d.Query("SELECT * FROM journal_entries")
-	for rows.Next() {
-		rows.Scan(&id, &date, &entry)
-		fmt.Println(strconv.Itoa(id) + ": " + date + " " + entry)
-	}
-}
-
-// Deletes the entire table of journal_entries
-func deleteTable(d *sql.DB) {
-	statement, _ := d.Prepare("DROP TABLE journal_entries")
-	statement.Exec()
-}
-
-// Deletes the entry of a particular date
-func deleteEntry(d *sql.DB) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input date of journal entry to delete:")
-	journalDate, _ := reader.ReadString('\n')
-
-	statement, _ := d.Prepare("DELETE FROM journal_entries WHERE date = ?")
-	statement.Exec(journalDate)
-}
-
-// Prints the entry of a particular date
-func searchEntry(d *sql.DB) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input date of journal entry to view:")
-	journalDate, _ := reader.ReadString('\n')
-	journalDate = journalDate[:len(journalDate)-1]
-
-	rows, _ := d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
-
-	for rows.Next() {
-		rows.Scan(&id, &date, &entry)
-		fmt.Println(strconv.Itoa(id) + ": " + date + " " + entry)
-	}
-}
-
-// Replaces the entry of a particular date
-func editEntry(d *sql.DB) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input date of journal entry to edit:")
-	journalDate, _ := reader.ReadString('\n')
-	journalDate = journalDate[:len(journalDate)-1]
-
-	rows, _ := d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
-	for rows.Next() {
-		rows.Scan(&id, &date, &entry)
-		fmt.Println(strconv.Itoa(id) + ": " + date + " " + entry)
-	}
-
-	fmt.Println("Input replacement entry:")
-	journalEntry, _ := reader.ReadString('\n')
-	journalEntry = journalEntry[:len(journalEntry)-1]
-	fmt.Println(journalEntry)
-
-	statement, _ := d.Prepare("UPDATE journal_entries SET entry = ? WHERE date = ?")
-	statement.Exec(journalEntry, journalDate)
-
-	rows, _ = d.Query("SELECT * FROM journal_entries WHERE date = ?", journalDate)
-	for rows.Next() {
-		rows.Scan(&id, &date, &entry)
-		fmt.Println(strconv.Itoa(id) + ": " + date + " " + entry)
-	}
 }
