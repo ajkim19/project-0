643516e51460bac2a7cc40bdfcb4d9fbc80e4ab5
ajkim19 <ajkim19@gmail.com>
2020-01-19 22:59:39 -0600

    Updated journal_test.go

--- GoJournal/pkg/journal/journal_test.go
+++ GoJournal/pkg/journal/journal_test.go
@@ -11,39 +11,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// func TestInputEntry(t *testing.T) {
-// 	database, err := sql.Open("sqlite3", "./journal_test.db")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	statement, err := database.Prepare("DROP TABLE IF EXISTS journal_entries")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS journal_entries (id INTEGER PRIMARY KEY, date TEXT, entry TEXT)")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	rescueStdout := os.Stdout
-// 	r, w, _ := os.Pipe()
-// 	os.Stdout = w
-
-// 	InputEntry(database)
-
-// 	w.Close()
-// 	out, _ := ioutil.ReadAll(r)
-// 	os.Stdout = rescueStdout
-
-// 	if string(out) != "01-01-2020: Today is New Year's Day!\n01-02-2020: It's the day after New Year's Day!\n" {
-// 		t.Error("Expected 01-01-2020: Today is New Year's Day!\n01-02-2020: It's the day after New Year's Day!\n")
-// 	}
-// }
-
 func TestViewEntireJournal(t *testing.T) {
 	database, err := sql.Open("sqlite3", "./journal_test.db")
 	if err != nil {
@@ -84,54 +51,9 @@ func TestViewEntireJournal(t *testing.T) {
 	fmt.Println(string(body))
 	w.Close()
 	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout //
+	os.Stdout = rescueStdout
 
-	if string(out) != "01-01-2020:\nToday is New Year's Day!\n01-02-2020:\nIt's the day after New Year's Day!\n" {
-		t.Error("Expected:\n01-01-2020:\nToday is New Year's Day!\n01-02-2020:\nIt's the day after New Year's Day!\n")
+	if string(out) != "\n01-01-2020:\nToday is New Year's Day!\n01-02-2020:\nIt's the day after New Year's Day!\n\n" {
+		t.Errorf("Got:\n%v\nExpected:\n01-01-2020:\nToday is New Year's Day!\n01-02-2020:\nIt's the day after New Year's Day!\n", string(out))
 	}
 }
-
-// func TestDeleteJournal(t *testing.T) {
-// 	database, err := sql.Open("sqlite3", "./journal_test.db")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	statement, err := database.Prepare("DROP TABLE IF EXISTS journal_entries")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS journal_entries (id INTEGER PRIMARY KEY, date TEXT, entry TEXT)")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	statement, err = database.Prepare(`INSERT INTO journal_entries (date, entry) VALUES ("01-01-2020", "Today is New Year's Day!")`)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	statement, err = database.Prepare(`INSERT INTO journal_entries (date, entry) VALUES ("01-02-2020", "It's the day after New Year's Day!")`)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	statement.Exec()
-
-// 	rescueStdout := os.Stdout
-// 	r, w, _ := os.Pipe()
-// 	os.Stdout = w
-
-// 	DeleteJournal(database)
-
-// 	w.Close()
-// 	out, _ := ioutil.ReadAll(r)
-// 	os.Stdout = rescueStdout
-
-// 	if string(out) != "" {
-// 		t.Error("Expected no output")
-// 	}
-// }
